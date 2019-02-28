fdd98583bdfe945678320d47fd89b9e9bdf65f31
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-28 13:00:40 -0500

    Added Navigation images and updated links

--- friends/src/components/Navigation.js
+++ friends/src/components/Navigation.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
+import styled from 'styled-components';
+
+import github from '../imgs/github-white.png';
+import linkedin from '../imgs/github-white.png';
+
+// Styled Components
 const NavWrapper = styled.div`
     width: 100%;
     background: #2C3539;
@@ -17,6 +22,7 @@ const NavContainer = styled.div`
     align-items: center;
 `;
 
+// Inline Styles
 const navLink = {
     color: '#ecf0f1',
     textDecoration: 'none',
@@ -24,18 +30,14 @@ const navLink = {
 }
 
 export default class Navigation extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
         <NavWrapper>
             <NavContainer>
                 <Link style={navLink} to='/'>Home</Link>
                 <Link style={navLink} to='/add'>Add Friend</Link>
-                <Link style={navLink} to='/add'><img src={link} alt="search" width='20' /></Link>
-                <Link style={navLink} to='/add'><img src={github} alt="search" width='20' /></Link>
+                <Link style={navLink} to='https://www.linkedin.com/in/guillermo-arria-devoe/'><img src={linkedin} alt="linkedin" width='20' /></Link>
+                <Link style={navLink} to='https://github.com/sapinspys'><img src={github} alt="github" width='20' /></Link>
             </NavContainer>
         </NavWrapper>
         )
