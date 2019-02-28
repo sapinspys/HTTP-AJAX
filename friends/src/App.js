faf1eb95b5ce6f90a4e93d8a9b3cc53598cc859e
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-02-28 12:50:01 -0500

    Added Route scaffolding to App component (STRETCH)

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import styled from 'styled-components';
 
+import Navigation from './components/Navigation';
 import FriendList from './components/FriendList';
+import FriendForm from './components/FriendForm';
 
+// Styled Components
 const PageContainer = styled.div`
   padding: 50px;
   background: #E8E8E8;
@@ -13,7 +17,10 @@ class App extends Component {
   render() {
     return (
       <PageContainer>
-        <FriendList />
+        <Navigation />
+        <Route exact path='/' component={FriendList} />
+        <Route path='/add' component={FriendForm} />
+        <Route path='/edit/:id' component={FriendForm} />
       </PageContainer>
     );
   }
